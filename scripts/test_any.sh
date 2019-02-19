26d6ac9b700544e16c1e462a751459d0dd062a25
Karthikeya Kaushik <Karthikeya.kaushik@logivations.com>
2019-02-19 15:38:15 +0100

    Fix cv2 bug. Include pylib.

--- scripts/test_any.sh
+++ scripts/test_any.sh
@@ -6,8 +6,8 @@ export CUDA_VISIBLE_DEVICES=$1
 python test_pixel_link_on_any_image.py \
             --checkpoint_path=$2 \
             --dataset_dir=$3 \
-            --eval_image_width=1280\
-            --eval_image_height=768\
-            --pixel_conf_threshold=0.5\
-            --link_conf_threshold=0.5\
+            --eval_image_width=740\
+            --eval_image_height=740\
+            --pixel_conf_threshold=0.3\
+            --link_conf_threshold=0.3\
             --gpu_memory_fraction=-1
